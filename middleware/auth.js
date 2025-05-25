00d85aeff86d5eb98402046368e8cfc49cff8986
pavan5125 <bollinenipavan512@gmail.com>
2025-05-26 00:05:45 +0530

    changes done

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,10 @@
 const jwt = require("jsonwebtoken");
 
+
 module.exports = function (req, res, next) {
-  const token = req.headers["authorization"]?.split(" ")[1];
+
+
+  const token = headers["authorization"]?.split(" ")[1];
   if (!token) return res.status(401).json({ message: "Access Denied" });
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
